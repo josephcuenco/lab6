5e64cf35347f5d42d8fca33b121ae424cb9ce754
josephcuenco <jjjcuenco777@gmail.com>
2023-03-07 16:41:56 -0500

    first commit

---
+++ password_encode.py
@@ -0,0 +1,41 @@
+# joseph cuenco
+
+def print_menu():
+    print('Menu')
+    print('-------------')
+    print('1. Encode')
+    print('2. Decode')
+    print('3. Quit')
+    print()
+
+
+def encode(password):
+    encoded_pass = ''
+    for i in password:
+        i = int(i)
+        i += 3
+        if i >= 10:
+            x = i % 10
+            i = x
+        i = str(i)
+        encoded_pass += i
+    return encoded_pass
+
+
+def main():
+    loop = True
+    while loop is True:
+
+        print_menu()
+
+        user_option = int(input('Please enter an option: '))
+
+        if user_option == 1:
+            pass_to_encode = input('Please enter your password to encode: ')
+            encoded_pass = encode(pass_to_encode)
+            print('Your password has been encoded and stored!')
+            print()
+
+
+if __name__ == '__main__':
+    main()
